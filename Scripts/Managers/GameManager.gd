b90a4410c6fa1960b5f289173552f73f6e52bb87
skitzaroo <angieshep315@gmail.com>
2025-04-13 16:35:17 -0400

    push

--- Scripts/Managers/GameManager.gd
+++ Scripts/Managers/GameManager.gd
@@ -1,6 +1,21 @@
-extends Node
+extends Node2D
 
+@export var upgrade_cost := 100
+@export var upgraded_texture: Texture2D
+@onready var sprite = $Sprite2D
 var money = 0
+var upgraded := false
+
+func _on_area_input_event(viewport, event, shape_idx):
+	if event.is_action_pressed("ui_accept") and not upgraded:
+		if GameManager.money >= upgrade_cost:
+			GameManager.add_money(-upgrade_cost)
+			sprite.texture = upgraded_texture
+			upgraded = true
+			print("🪑 Table upgraded!")
+		else:
+			print("❌ Not enough money")
+
 
 #NOTE This class is our game manager and handles the players money and loading scenes
 #These functions can be called globally from anywhere
